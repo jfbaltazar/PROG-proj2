61b2c8212623bb3e6acbfe1cd7bbf2381f604f77
João Baltazar <joaoferreirabaltazar@gmail.com>
2020-05-14 15:30:44 +0100

    14/5

--- Board.h
+++ Board.h
@@ -1,15 +1,20 @@
 #pragma once
 
+#include <string>
 #include <vector>
-#include "Word.h"
-#include <map>
-
 using namespace std;
 
 class Board {
 private:
-    //vector<Word> board{};
-    typedef map<const char[2], char> letter;
+    vector<vector<char>> board;
+    unsigned size;
 public:
+    const char letters[20] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'};
+    explicit Board(unsigned size);
+    char getLetter(const int &x, const int &y) const;
+    void setLetter(const int &x, const int &y, char letter);
+    unsigned getSize() const;
+    bool validPos(const int &x, const int &y) const;
+    void show(ostream &out) const;
 };
 
